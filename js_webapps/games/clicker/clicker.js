4fdaa1190ab4aae459901abe1c283e52f71af695
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2025-08-18 16:32:57 -0300

    Update clicker.js

--- js_webapps/games/clicker/clicker.js
+++ js_webapps/games/clicker/clicker.js
@@ -3,3 +3,6 @@ function clickFunction() {
   let clicks = clicks++
   document.getElementById("numberValue").textContent = clicks;
 }
+while true {
+  document.getElementById("btnClick").onclick = clickFunction()
+}
