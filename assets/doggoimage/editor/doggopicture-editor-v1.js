92d57f988a1643ac6900e6b765f9701421b4433b
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-02-07 14:07:35 -0400

    Create doggopicture-editor-v1.js

---
+++ assets/doggoimage/editor/doggopicture-editor-v1.js
@@ -0,0 +1,13 @@
+const colors = [
+  "#fff",
+  "#000",
+  "#ff0000",
+  "#00ff00",
+  "#0000ff",
+];
+
+function c(color) {
+  console.log('"' + colors[color] + '",');
+}
+
+// run this in the browser to make color data
