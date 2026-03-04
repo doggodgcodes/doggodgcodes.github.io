427179880b6af690ab465c3bb56e89e1ec79d5b9
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-04 08:17:29 -0400

    Create extra-doggodgcodes.js

---
+++ js_webapps/apps/aero-editor/extra-doggodgcodes.js
@@ -0,0 +1,2 @@
+console.info("Built in Aero Editor - this text is coming from a script to clarify where it was made.")
+console.info("You can find Aero Editor at https://doggodgcodes.github.io/js_webapps/apps/aero-editor")
