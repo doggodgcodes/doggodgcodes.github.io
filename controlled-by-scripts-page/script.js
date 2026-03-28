cd35b12a3329f2978362d34c38dadcf50c57f5ba
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-28 08:47:54 -0300

    Create script.js

---
+++ controlled-by-scripts-page/script.js
@@ -0,0 +1,22 @@
+const link = document.createElement("link");
+link.rel = "icon";
+link.type = "image/png";
+link.href = "https://yt3.googleusercontent.com/nMwX4c9fVcyxWNcVGKMsesmY3GU75jcDkrcK3ablWbd3xkpy1jxNTF5NXV29Miut87n31SskHw=s160-c-k-c0x00ffffff-no-rj";
+
+document.head.appendChild(link);
+// Create an h6 element
+const c = document.createElement("h6");
+
+// Set the text inside the h6 element
+c.textContent = "ᵈᵒᵍᵍᵒᵈᵍᶜᵒᵈᵉˢ ᶦˢ ᵒⁿ ᵗʰᶦˢ ᵖᵃᵍᵉ ⁽ᶦⁿ ᶜᵒᵈᵉ⁾";
+
+// Append the h6 element to the body
+document.body.appendChild(c);
+
+const scr = document.createElement("script");
+
+// Set the src inside the script element
+scr.src = "https://doggodgcodes.github.io/random/visit-metric.js";
+
+// Append the h6 element to the body
+document.body.appendChild(scr);
