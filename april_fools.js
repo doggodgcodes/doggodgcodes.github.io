da6620ffa2e1ccf98c342fb5f134182efd462790
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-28 15:38:50 -0300

    Create april_fools.js

---
+++ april_fools.js
@@ -0,0 +1,11 @@
+(function () {
+  const now = new Date();
+  const isAprilFirst = (now.getMonth() === 3 && now.getDate() === 1); // April = 3 (0-based)
+
+  if (isAprilFirst) {
+    setTimeout(() => {
+      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      console.log("From doggodgcodes: get rickrolled")
+    }, 5000); // 5000 ms = 5 seconds
+  }
+})();
