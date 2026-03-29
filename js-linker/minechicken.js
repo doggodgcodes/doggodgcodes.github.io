125eef1f59618d8df161aa7bc01f3daf688ceb49
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 13:56:27 -0300

    Update minechicken.js

--- js-linker/minechicken.js
+++ js-linker/minechicken.js
@@ -18,15 +18,7 @@ document.body.appendChild(brk2)
 
 const div = document.createElement("div")
 
-div.innerHTML = "
-  <a href="https://doggodgcodes.github.io" class="project-card">
-                    <h2 class="project-title">DoggoDGCodes Homepage</h2>
-                    <p class="project-description" style="color: navy;">
-                        Travel to doggodgcodes.github.io to pack a punch made of enjoyability! Games like 98 Clicker, and The Maze are enjoyable experiences for when you get bored (why would you get bored here, it's awesome here) where you can explore every inch or centimeter of it!
-                    </p>
-                    <span class="project-link">View Site →</span>
-                </a>
-  "
+div.innerHTML = "<a href="https://doggodgcodes.github.io" class="project-card"><h2 class="project-title">DoggoDGCodes Homepage</h2><p class="project-description" style="color: navy;">Travel to doggodgcodes.github.io to pack a punch made of enjoyability! Games like 98 Clicker, and The Maze are enjoyable experiences for when you get bored (why would you get bored here, it's awesome here) where you can explore every inch or centimeter of it!</p><span class="project-link">View Site →</span></a>"
 
 
 document.body.appendChild(div)
