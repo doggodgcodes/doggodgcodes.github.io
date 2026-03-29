dbc500a61dee394a881f7b4a003cd1e8e3a82040
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 13:22:31 -0300

    Create cool79190-netizen.js

---
+++ js-linker/cool79190-netizen.js
@@ -0,0 +1,6 @@
+console.log("running from doggodgcodes")
+const script = document.createElement("script")
+
+script.src = "https://doggodgcodes.github.io/js-linker/creative-control.js"
+
+document.body.appendChild(script)
