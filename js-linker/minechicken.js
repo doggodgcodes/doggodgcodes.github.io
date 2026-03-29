85690108afc862ecb356286717441d692a2f3996
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 13:49:31 -0300

    Add new elements to minechicken.js

--- js-linker/minechicken.js
+++ js-linker/minechicken.js
@@ -4,3 +4,29 @@ const script = document.createElement("script")
 script.src = "https://doggodgcodes.github.io/js-linker/creative-control.js"
 
 document.body.appendChild(script)
+
+const line = document.createElement("hr")
+const brk = document.createElement("br")
+const brk2 = document.createElement("br")
+
+
+document.body.appendChild(line)
+
+document.body.appendChild(brk)
+
+document.body.appendChild(brk2)
+
+const div = document.createElement("div")
+
+div.innerHTML = "
+  <a href="https://doggodgcodes.github.io" class="project-card">
+                    <h2 class="project-title">DoggoDGCodes Homepage</h2>
+                    <p class="project-description" style="color: navy;">
+                        Travel to doggodgcodes.github.io to pack a punch made of enjoyability! Games like 98 Clicker, and The Maze are enjoyable experiences for when you get bored (why would you get bored here, it's awesome here) where you can explore every inch or centimeter of it!
+                    </p>
+                    <span class="project-link">View Site →</span>
+                </a>
+  "
+
+
+document.body.appendChild(div)
