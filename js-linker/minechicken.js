c09fc97f3af0df66029d973d6c5a33540afe0edb
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 15:05:26 -0300

    Update minechicken.js

--- js-linker/minechicken.js
+++ js-linker/minechicken.js
@@ -26,8 +26,8 @@ div.innerHTML = `
 <div class="projects">
 <a href="https://doggodgcodes.github.io" class="project-card"><h2 class="project-title">DoggoDGCodes Homepage</h2><p class="project-description" style="color: navy;">Travel to doggodgcodes.github.io to pack a punch made of enjoyability! Games like 98 Clicker, and The Maze are enjoyable experiences for when you get bored (why would you get bored here, it's awesome here) where you can explore every inch or centimeter of it!</p><span class="project-link">View Site →</span></a>
 <a href="https://doggodgcodes.github.io/ai/" class="project-card"><h2 class="project-title">DoggoDGCodes AI Page</h2><p class="project-description" style="color: navy;">Explore hilariously random AI creations made by doggodgcodes and others to test your AI detection skills.</p><span class="project-link">View Page →</span></a>
-<a href="https://github.com/doggodgcodes" class="project-card"><h2 class="project-title">DoggoDGCodes Code</h2><p class="project-description" style="color: navy;">Explore doggodgcodes code!</p><span class="project-link">View Site →</span></a>
-<a href="https://github.com/minechicken98" class="project-card"><h2 class="project-title">MineChicken98's Code</h2><p class="project-description" style="color: navy;">Explore minechicken98's code1</p><span class="project-link">View Site →</span></a>
+<a href="https://github.com/doggodgcodes" class="project-card"><h2 class="project-title">DoggoDGCodes Code</h2><p class="project-description" style="color: navy;">Explore doggodgcodes code!</p><span class="project-link">View Page →</span></a>
+<a href="https://github.com/minechicken98" class="project-card"><h2 class="project-title">MineChicken98's Code</h2><p class="project-description" style="color: navy;">Explore minechicken98's code1</p><span class="project-link">View Page →</span></a>
 <img class="dg-fbar-img" src="https://i.chzbgr.com/full/10555248128/hEA6A8B63/will-open-this-coca-co-1127">
 <img class="dg-fbar-img" src="https://doggodgcodes.github.io/assets/memes/tony.jpg">
 <img class="dg-fbar-img" src="https://doggodgcodes.github.io/assets/memes/cat.png">
@@ -54,4 +54,4 @@ script2.src = "https://doggodgcodes.github.io/js-linker/trail.js";
 
 document.body.appendChild(script2);
 
-console.log("Appended Trail.js")
+console.log("Appended Trail.js");
