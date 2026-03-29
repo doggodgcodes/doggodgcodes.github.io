f80666e37b559447bbac82bc1167a67b1b486e2f
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 14:31:34 -0300

    Update additional content

--- js-linker/minechicken.js
+++ js-linker/minechicken.js
@@ -21,11 +21,14 @@ console.log("Appended Horizontal Line");
 const div = document.createElement("div");
 
 div.innerHTML = `
-<div class="container"><h4>DoggoDGCodes Section <small>(injected into homepage)</small></h4></div>
+<div class="container"><h1>DoggoDGCodes Section <small>(injected into homepage)</small></h1>
+<div class="projects">
 <a href="https://doggodgcodes.github.io" class="project-card"><h2 class="project-title">DoggoDGCodes Homepage</h2><p class="project-description" style="color: navy;">Travel to doggodgcodes.github.io to pack a punch made of enjoyability! Games like 98 Clicker, and The Maze are enjoyable experiences for when you get bored (why would you get bored here, it's awesome here) where you can explore every inch or centimeter of it!</p><span class="project-link">View Site →</span></a>
 <a href="https://doggodgcodes.github.io/ai/" class="project-card"><h2 class="project-title">DoggoDGCodes AI Page</h2><p class="project-description" style="color: navy;">Explore hilariously random AI creations made by doggodgcodes and others to test your AI detection skills</p><span class="project-link">View Page →</span></a>
 <a href="https://github.com/doggodgcodes" class="project-card"><h2 class="project-title">DoggoDGCodes Code</h2><p class="project-description" style="color: navy;">Explore doggodgcodes code.</p><span class="project-link">View Site →</span></a>
 <a href="https://github.com/minechicken98" class="project-card"><h2 class="project-title">MineChicken98's Code</h2><p class="project-description" style="color: navy;">Explore minechicken98's code.</p><span class="project-link">View Site →</span></a>
+</div>
+</div>
 `;
 
 
