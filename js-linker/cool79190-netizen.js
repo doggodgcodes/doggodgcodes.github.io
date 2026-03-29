74120b6acbae1d56dfef2e4f9b8ed6562dad045a
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 14:02:50 -0300

    Update cool79190-netizen.js

--- js-linker/cool79190-netizen.js
+++ js-linker/cool79190-netizen.js
@@ -1,6 +1,14 @@
-console.log("running from doggodgcodes")
-const script = document.createElement("script")
+console.log("running from doggodgcodes");
+const script = document.createElement("script");
 
-script.src = "https://doggodgcodes.github.io/js-linker/creative-control.js"
+script.src = "https://doggodgcodes.github.io/js-linker/creative-control.js";
 
-document.body.appendChild(script)
+document.body.appendChild(script);
+
+const h = document.createElement("hr");
+const text = document.createElement("h4");
+
+text.textContent = "Hi from doggodgcodes script!";
+
+document.body.appendChild(h);
+document.body.appendChild(text);
