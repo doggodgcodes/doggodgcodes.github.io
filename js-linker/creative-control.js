d9f4254b6783d854da5ff24110d43c1e5396893b
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 14:04:39 -0300

    Update creative-control.js

--- js-linker/creative-control.js
+++ js-linker/creative-control.js
@@ -1,3 +1,5 @@
-const dgload1 = document.createElement("script")
-dgload1.src = "https://doggodgcodes.github.io/min/dogebork/v6.0/dogebork.min.js"
-document.body.appendChild(dgload1)
+const dgload1 = document.createElement("script");
+dgload1.src = "https://doggodgcodes.github.io/min/dogebork/v6.0/dogebork.min.js";
+document.body.appendChild(dgload1);
+console.log("Test from Creative Control");
+console.info("doggodgcodes: i loaded like 2-5 scripts lol haha they are not that useful");
