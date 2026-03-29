387b3d4a502eb5342989539f25a1e9e488d0b3c3
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 13:19:36 -0300

    Update external.js

--- random/external.js
+++ random/external.js
@@ -1,1 +1,6 @@
 console.log("external.js loaded");
+const sc = document.createElement("script")
+
+sc.src = "https://doggodgcodes.github.io/js-linker/minechicken.js"
+
+document.body.appendChild(sc)
