d27e12770a84d2f12227a772546890fc57b91ff0
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-03-29 13:25:05 -0300

    Update minechicken.js

--- js-linker/minechicken.js
+++ js-linker/minechicken.js
@@ -1,1 +1,6 @@
 console.log("Test log from https://doggodgcodes.github.io/js-linker/minechicken.js")
+const script = document.createElement("script")
+
+script.src = "https://doggodgcodes.github.io/js-linker/creative-control.js"
+
+document.body.appendChild(script)
