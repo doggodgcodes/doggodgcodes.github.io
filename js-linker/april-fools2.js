ddb1b4cff07636a544bb878918d2d9e8da45034f
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-04-11 09:48:29 -0300

    Create april-fools2.js

---
+++ js-linker/april-fools2.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+  const today = new Date();
+
+  // April = 3 (months are 0-based: Jan = 0)
+if (today.getMonth() === today.getMonth() && today.getDate() === today.getDate()) {
+
+const glitchCss = document.createElement("link");
+glitchCss.rel = "stylesheet";
+glitchCss.href = "https://doggodgcodes.github.io/mine-chicken-dg/css-mine-chicken/glitchazon.css";
+document.head.appendChild(glitchCss);
+document.title = "⚠️ CRITICAL FOOLING IN PROGRESS ⚠️";
+
+console.log("get fooled for april fools haha");
+document.body.innerHTML = `
+  <img src="https://doggodgcodes.github.io/assets/prank/invalid.png" style="width: 50vw; height: 50vh; object-fit: cover;">
+  <h1>APRIL FOOLS DINGUS HAHAHAHAHAHHAHAHAHAHAHAHAHHAHAH</h1>
+  `;
+}
+});
