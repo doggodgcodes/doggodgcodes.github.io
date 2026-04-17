aba4ace8e2ae5edf947bdf9a9e9043e6061669de
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-04-17 16:23:05 -0300

    Update premuim900.js

--- js-linker/premuim900.js
+++ js-linker/premuim900.js
@@ -1,2 +1,8 @@
+const dgDiv = document.getElementById("doggodgcodes-div")
+
 console.log("JS linker from doggodgcodes connected!")
 console.log("sup hamburgers")
+
+dgdiv.innerHTML = `
+<h4>doggodgcodes div</h4>
+`
