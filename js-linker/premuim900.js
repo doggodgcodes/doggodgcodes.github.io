b1f50e51f2629049ce4c6e053aec4dfa6556865f
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-04-19 12:15:59 -0300

    Add new area from minechicken

--- js-linker/premuim900.js
+++ js-linker/premuim900.js
@@ -12,8 +12,40 @@ css.rel = "stylesheet";
 css.href = "https://doggodgcodes.github.io/js-linker/premuim900.css";
 document.head.append(css);
 
+const css2 = document.createElement("link");
+css2.rel = "stylesheet";
+css2.href = "https://doggodgcodes.github.io/mine-chicken-dg/app.css";
+document.head.append(css2);
+
+const css3 = document.createElement("link");
+css3.rel = "stylesheet";
+css3.href = "https://doggodgcodes.github.io/mine-chicken-dg/homepage.css";
+document.head.append(css3);
+
 const script2 = document.createElement("script");
 
 script2.src = "https://doggodgcodes.github.io/js-linker/trail.js";
 
 document.body.appendChild(script2);
+
+const div = document.createElement("div");
+
+div.innerHTML = `
+<div class="container"><h1>DoggoDGCodes Section</h1>
+<p class="subtitle">*this section was injected into homepage</p>
+<div class="projects">
+<a href="https://doggodgcodes.github.io" target="_blank" class="project-card"><h2 class="project-title">DoggoDGCodes Homepage</h2><p class="project-description" style="color: navy;">Travel to doggodgcodes.github.io to pack a punch made of enjoyability! Games like 98 Clicker, and The Maze are enjoyable experiences for when you get bored (why would you get bored here, it's awesome here) where you can explore every inch or centimeter of it!</p><span class="project-link">View Site →</span></a>
+<a href="https://doggodgcodes.github.io/ai/" target="_blank" class="project-card"><h2 class="project-title">DoggoDGCodes AI Page</h2><p class="project-description" style="color: navy;">Explore hilariously random AI creations made by doggodgcodes and others to test your AI detection skills.</p><span class="project-link">View Page →</span></a>
+<a href="https://github.com/doggodgcodes" target="_blank" class="project-card"><h2 class="project-title">DoggoDGCodes Code</h2><p class="project-description" style="color: navy;">Explore doggodgcodes code!</p><span class="project-link">View Page →</span></a>
+<a href="https://github.com/minechicken98" target="_blank" class="project-card"><h2 class="project-title">MineChicken98's Code</h2><p class="project-description" style="color: navy;">Explore minechicken98's code!</p><span class="project-link">View Page →</span></a>
+<a href="https://doggodgcodes.github.io/random/" target="_blank" class="project-card"><h2 class="project-title">DoggoDGCodes Random Page</h2><p class="project-description" style="color: navy;">Find new favorite webpage! Has a random website picker, that is truly random!</p><span class="project-link">View Page →</span></a>
+<img class="dg-fbar-img" src="https://i.chzbgr.com/full/10555248128/hEA6A8B63/will-open-this-coca-co-1127">
+<img class="dg-fbar-img" src="https://doggodgcodes.github.io/assets/memes/tony.jpg">
+<img class="dg-fbar-img" src="https://doggodgcodes.github.io/assets/memes/cat.png">
+</div>
+</div>
+`;
+
+
+
+document.body.appendChild(div);
