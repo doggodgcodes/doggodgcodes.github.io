67b59d69ffab5905ab0d886a0db5ddc4422f6689
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-04-20 16:00:31 -0300

    Update shutter.js (disable it)

--- premuim900/shutter.js
+++ premuim900/shutter.js
@@ -1,6 +1,6 @@
-const on = true;
+const on = false;
 if (on) {
   document.innerHTML = "<h1>Site currently off for now. (shutter.js test)</h1>"
 } else {
-  console.info("Status: Site is online. From https://doggodgcodes.github.io/premuim900/shutter.js")
+  console.info("Status: Site is online. Probably won't be used. From https://doggodgcodes.github.io/premuim900/shutter.js")
 }
