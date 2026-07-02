5e3dc84b8b31da774625a951e471df7d43362bae
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-07-02 09:07:14 -0300

    Update kowyism.js

--- js-linker/kowyism.js
+++ js-linker/kowyism.js
@@ -1,4 +1,4 @@
 console.log("doggodgcodes script loaded");
 let iframe = document.createElement("iframe");
 iframe.src = "https://minechicken98.github.io/pokemon-frost/";
-body.appendChild(iframe);
+document.body.appendChild(iframe);
