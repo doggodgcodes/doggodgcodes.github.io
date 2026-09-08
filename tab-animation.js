fdc1d9945574df71bfd9fe285045d826ba115419
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-09-08 10:55:03 -0300

    Checkbox (Copilot fixed some stuff)

--- tab-animation.js
+++ tab-animation.js
@@ -1,14 +1,43 @@
-frame = ["doggodgcodes", "d oggodgcodes", "do ggodgcodes", "dog godgcodes", "dogg odgcodes", "doggo dgcodes", "doggod gcodes", "doggodg codes", "doggodgc odes", "doggodgco des", "doggodgcod es", "doggodgcode s"];
-i = 0;
+const frame = ["doggodgcodes", "d oggodgcodes", "do ggodgcodes", "dog godgcodes", "dogg odgcodes", "doggo dgcodes", "doggod gcodes", "doggodg codes", "doggodgc odes", "doggodgco des", "doggodgcod es", "doggodgcode s"];
+const originalTitle = document.title;
+let i = 0;
+let animationEnabled = true;
+let animationTimer;
 
 function tab() {
-  document.title = frame[i];
-  if (i < frame.length - 1) {
-    i++;
-  } else {
-    i = 0;
+  if (!animationEnabled) {
+    return;
   }
-  setTimeout(tab, 100);
+
+  if (document.hidden) {
+    document.title = frame[i];
+    if (i < frame.length - 1) {
+      i++;
+    } else {
+      i = 0;
+    }
+  }
+  animationTimer = setTimeout(tab, 100);
 }
 
-tab()
+document.addEventListener("DOMContentLoaded", () => {
+  const animationCheckbox = document.getElementById("animationCheckbox");
+
+  if (!animationCheckbox) {
+    return;
+  }
+
+  animationCheckbox.checked = animationEnabled;
+  animationCheckbox.addEventListener("change", () => {
+    animationEnabled = animationCheckbox.checked;
+
+    if (animationEnabled) {
+      tab();
+    } else {
+      clearTimeout(animationTimer);
+      document.title = originalTitle;
+    }
+  });
+});
+
+tab();
