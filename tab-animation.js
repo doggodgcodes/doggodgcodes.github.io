8942341277f8781741794592ba6f4c8c5b99b1fc
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-09-08 12:56:31 -0300

    Debug log (seriously, did copilot write this right or did i mess it up)

--- tab-animation.js
+++ tab-animation.js
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
   animationCheckbox.checked = animationEnabled;
   animationCheckbox.addEventListener("change", () => {
     animationEnabled = animationCheckbox.checked;
+    console.log(animationEnabled)
 
     if (animationEnabled == true) {
       tab();
